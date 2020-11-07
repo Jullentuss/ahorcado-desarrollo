4b7469411f892eefd40ca80dea3eda3f6e2ab2b6
pjvargas12 <pjvargas1296@gmail.com>
2020-11-06 19:26:14 -0500

    Segundo Commit

--- js/controlador.js
+++ js/controlador.js
@@ -1,8 +1,31 @@
 function funcionInicial(){
-    
+    $("#btnBuscar").on('click', buscarPelicula);
 }
+
 function buscarPelicula(){
-    console.log($("#inputBuscador").val());
+	var pelicula = $("#inputBuscador").val();
+    console.log(pelicula);
+    if (pelicula.length == 0) {
+    	alert('Ingrese un nombre de película');
+    } else {
+    	cargarPelicula(pelicula);
+    }
+}
+
+function cargarPelicula(nombre){
+	console.log('Cargar película '+nombre);
+	const apiKey = '42af3ea1';
+	$.ajax({
+		url: 'http://www.omdbapi.com/?apikey='+apiKey+'&s='+nombre,
+		dataType: 'json',
+		type: 'GET',
+		success: (respuesta) => {
+			console.log(respuesta);
+		},
+		error: (respuestaError) => {
+			console.log(respuestaError);
+		}
+	})
 }
 
 /*const funcionInicial = () =>{
